882543da2dc962085ebc79d8e6986b51a8e3d548
Riya2178 <mswarnali21@gmail.com>
2024-09-08 18:53:30 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,154 @@
+
+function ToggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+window.addEventListener("scroll",function(){
+    var header = document.querySelector("header");
+    header.classList.toggle('sticky',window.scrollY > 0);
+    
+})
+
+
+
+
+
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthitems = items.length - 1;
+
+next.onclick = function() {
+    if (active + 1 > lengthitems) {
+        active = 0;
+    } else {
+        active = active + 1;
+    }
+    reloadslider();
+}
+
+prev.onclick = function() {
+    if (active - 1 < 0) {
+        active = lengthitems;
+    } else {
+        active = active - 1;
+    }
+    reloadslider();
+}
+
+let autoslide = setInterval(() => { next.click(); }, 3000);
+
+function reloadslider() {
+    let checkleft = items[active].offsetLeft;
+    list.style.left = -checkleft + 'px';
+
+    let lastactiveDot = document.querySelector('.slider .dots li.active');
+    if (lastactiveDot) lastactiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function() {
+        active = key;
+        reloadslider();
+    })
+})
+
+
+function initMap() {
+    const bounds = new google.maps.LatLngBounds();
+    const markersArray = [];
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 55.53, lng: 9.4 },
+      zoom: 10,
+    });
+    // initialize services
+    const geocoder = new google.maps.Geocoder();
+    const service = new google.maps.DistanceMatrixService();
+    // build request
+    const origin1 = { lat: 55.93, lng: -3.118 };
+    const origin2 = "Greenwich, England";
+    const destinationA = "Stockholm, Sweden";
+    const destinationB = { lat: 50.087, lng: 14.421 };
+    const request = {
+      origins: [origin1, origin2],
+      destinations: [destinationA, destinationB],
+      travelMode: google.maps.TravelMode.DRIVING,
+      unitSystem: google.maps.UnitSystem.METRIC,
+      avoidHighways: false,
+      avoidTolls: false,
+    };
+  
+    // put request on page
+    document.getElementById("request").innerText = JSON.stringify(
+      request,
+      null,
+      2
+    );
+    // get distance matrix response
+    service.getDistanceMatrix(request).then((response) => {
+      // put response
+      document.getElementById("response").innerText = JSON.stringify(
+        response,
+        null,
+        2
+      );
+  
+      // show on map
+      const originList = response.originAddresses;
+      const destinationList = response.destinationAddresses;
+  
+      deleteMarkers(markersArray);
+  
+      const showGeocodedAddressOnMap = (asDestination) => {
+        const handler = ({ results }) => {
+          map.fitBounds(bounds.extend(results[0].geometry.location));
+          markersArray.push(
+            new google.maps.Marker({
+              map,
+              position: results[0].geometry.location,
+              label: asDestination ? "D" : "O",
+            })
+          );
+        };
+        return handler;
+      };
+  
+      for (let i = 0; i < originList.length; i++) {
+        const results = response.rows[i].elements;
+  
+        geocoder
+          .geocode({ address: originList[i] })
+          .then(showGeocodedAddressOnMap(false));
+  
+        for (let j = 0; j < results.length; j++) {
+          geocoder
+            .geocode({ address: destinationList[j] })
+            .then(showGeocodedAddressOnMap(true));
+        }
+      }
+    });
+  }
+  
+  function deleteMarkers(markersArray) {
+    for (let i = 0; i < markersArray.length; i++) {
+      markersArray[i].setMap(null);
+    }
+  
+    markersArray = [];
+  }
+  
+  window.initMap = initMap;
+
+
+//footer js
+
+
+
+  
---
+++ module.js
@@ -0,0 +1,29 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 22.583369551073677, lng: 88.48696703800968 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 17,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+
+
+initMap();
