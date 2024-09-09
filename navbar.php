7d5ee1f7e77019116e5a37ed3d114dddaae19b00
Priya7319 <121280644+Priya7319@users.noreply.github.com>
2024-09-09 15:52:43 +0530

    import aos and initial configuration

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
 <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="./docs/images/logo/logo.png" class="h-8" >
+      <img src="./docs/images/logo/logo.png" class="h-12 w-12" >
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AgroFarm</span>
   </a>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
